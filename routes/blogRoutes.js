ea51ef44fb45d35e336efcdc48e64359f892075b
Hasib <420hasibulhasan@gmail.com>
2025-11-20 05:25:22 +0600

    added blog delete function

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getDB } from "../config/db.js";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
@@ -48,5 +49,49 @@ router.get("/", async (req, res) => {
     res.status(500).send({ error: "Failed to fetch blogs" });
   }
 });
+
+ 
+router.delete("/delete-all", async (req, res) => {
+  try {
+    const db = getDB();
+    const blogCollection = db.collection("blogs");
+
+    await blogCollection.deleteMany({});
+
+    res.status(200).send({ message: "All blog posts have been deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting blogs:", error);
+    res.status(500).send({ error: "Failed to delete blog posts" });
+  }
+});
+
+
+router.delete("/delete", async (req, res) => {
+  const { id } = req.query;
+
+  if (!id) {
+    return res.status(400).send({ error: "Title is required in query" });
+  }
+
+  try {
+    const db = getDB();
+    const blogCollection = db.collection("blogs");
+// console.log(id,"id") 
+     // Convert string id → ObjectId
+    const objectId = new ObjectId(id);
+
+    const result = await blogCollection.deleteOne({ _id: objectId });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).send({ message: "No blog found with this title" });
+    }
+
+    res.status(200).send({ message: "Blog deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting blog:", error);
+    res.status(500).send({ error: "Failed to delete blog" });
+  }
+});
+
 
 export default router;
